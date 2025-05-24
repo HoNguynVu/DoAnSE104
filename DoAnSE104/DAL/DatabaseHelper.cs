b6219bff02ce95555cd6904e5597ac1998646308
quangvinh263 <23521792@gm.uit.edu.vn>
2025-05-24 09:45:52 +0700

    3-layer and add DatabaseHelper

---
+++ DoAnSE104/DAL/DatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DoAnSE104.DAL
+{
+    class DatabaseHelper
+    {
+        private string connectionString = "server=localhost;database=DaiLyDB;user=root;password=;";
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}

df2d1a824b752b1e68ac90f2cbce0fb7d1d68daf
Ho Nguyen Vu <23521805@gm.uit.edu.vn>
2025-05-24 13:29:06 +0700

    "them cac DAL cho sp2"

---
+++ DoAnSE104/DAL/DAL_CachDung.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using DoAnSE104.DTO;
+
+namespace DoAnSE104.DAL
+{
+    class DAL_CachDung
+    {
+        DatabaseHelper DatabaseHelper = new DatabaseHelper();
+        public List<DTO_CachDung> LayDanhSachCachDung()
+        {
+            string query = "SELECT TenCachDung FROM CACHDUNG";
+            DataTable dt = DatabaseHelper.ExecuteQuery(query);
+            List<DTO_CachDung> danhSachCachDung = new List<DTO_CachDung>();
+            foreach (DataRow row in dt.Rows)
+            {
+                string maCachDung = row["MaCachDung"].ToString();
+                string tenCachDung = row["TenCachDung"].ToString();
+                DTO_CachDung cachDung = new DTO_CachDung(maCachDung, tenCachDung);
+                danhSachCachDung.Add(cachDung);
+            }
+            return danhSachCachDung;
+        }
+    }
+}
---
+++ DoAnSE104/DAL/DAL_DonVi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using DoAnSE104.DTO;
+
+namespace DoAnSE104.DAL
+{
+    class DAL_DonVi
+    {
+        DatabaseHelper DatabaseHelper = new DatabaseHelper();
+        public List<DTO_DonVi> LayDanhSachDonVi()
+        {
+            string query = "SELECT TenDonVi FROM DONVI";
+            DataTable dt = DatabaseHelper.ExecuteQuery(query);
+            List<DTO_DonVi> danhSachDonVi = new List<DTO_DonVi>();
+            foreach (DataRow row in dt.Rows)
+            {
+                string maDonVi = row["MaDonVi"].ToString();
+                string tenDonVi = row["TenDonVi"].ToString();
+                DTO_DonVi donVi = new DTO_DonVi(maDonVi, tenDonVi);
+                danhSachDonVi.Add(donVi);
+            }
+            return danhSachDonVi;
+        }
+    }
+}

ced0a0d53eb7402b202b456ce0c13e872ccb116e
Le Minh Khoi <leminhkhoi36@gmail.com>
2025-05-30 02:39:18 +0700

    feature: add events ui-home

--- DoAnSE104/GUI/GUI_Home.cs
+++ DoAnSE104/GUI/GUI_Home.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DoAnSE104.GUI
@@ -15,18 +9,32 @@ namespace DoAnSE104.GUI
         public GUI_Home()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.WhiteSmoke;
         }
 
         private void BtnMedicalExamination_Click(object sender, EventArgs e)
         {
             GUI_TiepNhanBenhNhan gui_TiepNhanBenhNhan = new GUI_TiepNhanBenhNhan();
-            gui_TiepNhanBenhNhan.Show();
+            gui_TiepNhanBenhNhan.ShowDialog();
         }
 
         private void BtnCreateMedicalExamination_Click(object sender, EventArgs e)
         {
             GUI_LapPhieuKhamBenh gui_LapPhieuKhamBenh = new GUI_LapPhieuKhamBenh();
-            gui_LapPhieuKhamBenh.Show();
+            gui_LapPhieuKhamBenh.ShowDialog();
+        }
+
+        private void BtnPatientLookup_Click(object sender, EventArgs e)
+        {
+            GUI_TraCuuBenhNhan gui_TraCuuBenhNhan = new GUI_TraCuuBenhNhan();
+            gui_TraCuuBenhNhan.ShowDialog();
+        }
+
+        private void BtnInvoicePayment_Click(object sender, EventArgs e)
+        {
+            GUI_LapHoaDon gui_LapHoaDon = new GUI_LapHoaDon();
+            gui_LapHoaDon.ShowDialog();
         }
     }
 }

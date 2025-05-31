94a799a9755e8459c7d4dd8579773a8275e28a58
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-31 17:30:19 +0700

    Restore changes from commit fc9f7e8

---
+++ DoAnSE104/BUS/BUS_BaoCaoSuDungThuoc.cs
@@ -0,0 +1,26 @@
+﻿using DoAnSE104.DAL;
+using DoAnSE104.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnSE104.BUS
+{
+    public class BUS_BaoCaoSuDungThuoc
+    {
+        DAL_BaoCaoSoLuongThuoc dal = new DAL_BaoCaoSoLuongThuoc();
+
+        public List<DTO_BaoCaoSuDungThuoc> LayBaoCaoSuDungThuoc(int month, int year)
+        {
+            if (month < 1 || month > 12)
+                throw new ArgumentException("Tháng không hợp lệ.");
+
+            if (year < 2000 || year > DateTime.Now.Year)
+                throw new ArgumentException("Năm không hợp lệ.");
+
+            return dal.LayBaoCaoSuDungThuoc(month, year);
+        }
+    }
+}
---
+++ DoAnSE104/DTO/DTO_BaoCaoSuDungThuoc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnSE104.DTO
+{
+    public class DTO_BaoCaoSuDungThuoc
+    {
+        public int soLuong {  get; set; }
+        public string donViTinh {  get; set; }
+        public string tenThuoc {  get; set; }
+        public int soLanDung {  get; set; }
+        public DTO_BaoCaoSuDungThuoc() { }
+
+        public DTO_BaoCaoSuDungThuoc(int sl, string dvt, string ten, int sld)
+        {
+            soLuong = sl;
+            donViTinh = dvt;
+            tenThuoc = ten;
+            soLanDung = sld;
+        }
+    }
+
+}

3a46df211884ff7867e23320a14c5fe459eacf74
Ho Nguyen Vu <23521805@gm.uit.edu.vn>
2025-05-31 22:06:21 +0700

    pull

--- DoAnSE104/DAL/DatabaseHelper.cs
+++ DoAnSE104/DAL/DatabaseHelper.cs
@@ -10,13 +10,12 @@ namespace DoAnSE104.DAL
 {
     class DatabaseHelper
     {
-<<<<<<< HEAD
-        private string connectionString = "server=localhost;database=PhongKham;user=root;password=123456;";
-=======
 
-        private string connectionString = "server=localhost;database=PhongKham;user=root;password=22022005;";
+        private string connectionString = "server=localhost;database=PhongKham;user=root;password=vudz1234;";
 
->>>>>>> 27c29f16b05762ce0d7abbdb4f7cbf1ba5c70809
+
+
+
         public MySqlConnection GetConnection()
         {
             return new MySqlConnection(connectionString);

1d8c509b67ed62711755b463f42a878f7093d748
loihnt05 <23520869@gm.uit.edu.vn>
2025-06-02 06:34:13 +0700

    fix dal in quanlythamso + quanlyloaibenh

--- DoAnSE104/GUI/GUI_LapHoaDon.cs
+++ DoAnSE104/GUI/GUI_LapHoaDon.cs
@@ -1,13 +1,8 @@
 ﻿using DoAnSE104.BUS;
 using DoAnSE104.DTO;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DoAnSE104.GUI
--- DoAnSE104/GUI/GUI_QuanLyDonViTinh.cs
+++ DoAnSE104/GUI/GUI_QuanLyDonViTinh.cs
@@ -8,7 +8,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DoAnSE104.GUI
